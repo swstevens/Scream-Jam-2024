d0d039f785c90c85d2001fa553bae33fa6418c5e
swstevens <sstevensw@gmail.com>
2024-10-16 15:28:46 -0700

    movement limited to 1 now working

--- scream-jam-2024/node_2d.gd
+++ scream-jam-2024/node_2d.gd
@@ -1,29 +1,20 @@
 extends Node2D
 
-var astar_grid: AStarGrid2D
 @onready var ground: TileMapLayer = $"../Ground"
+@onready var base: Node2D = $".."
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	astar_grid = AStarGrid2D.new()
-	astar_grid.region = ground.get_used_rect()
-	astar_grid.cell_size = Vector2(32,16)
-	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
-	astar_grid.update()
+	pass
 
 func _input(event):
 
 	if event.is_action_pressed("move") == false:
 		return
-	print(		ground.local_to_map(global_position),
-	ground.local_to_map(get_global_mouse_position()))
-	var id_path = astar_grid.get_id_path(
-		ground.local_to_map(global_position),
-		ground.local_to_map(get_global_mouse_position())
-	)
-	print(ground.map_to_local(ground.local_to_map(get_global_mouse_position())))
-	position = ground.map_to_local(ground.local_to_map(get_global_mouse_position()))
-	position.x -= 160
-	position.y -= 120 + 8
-	print(id_path)
+	var apple: Vector2i = ground.local_to_map(base.to_local(get_global_mouse_position()))
+	var beta: Vector2i = ground.local_to_map(base.to_local(global_position))
+	print(apple,beta)
+	if abs(apple.x - beta.x) <= 1 and abs(apple.y - beta.y) <= 1:
+		# also have the broadcast move to the enemies here
+		position = ground.map_to_local(ground.local_to_map(base.to_local(get_global_mouse_position())))
 		
