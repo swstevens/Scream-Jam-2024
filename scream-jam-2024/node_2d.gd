65a876ba716e548cd95f236f87ad448cb72f9a55
Troy Clendenen <33107696+tclendo@users.noreply.github.com>
2024-10-16 16:34:11 -0700

    Add check for tile texture to deny movement

--- scream-jam-2024/node_2d.gd
+++ scream-jam-2024/node_2d.gd
@@ -11,11 +11,17 @@ func _input(event):
 
 	if event.is_action_pressed("move") == false:
 		return
+	
 	var apple: Vector2i = ground.local_to_map(base.to_local(get_global_mouse_position()))
 	var beta: Vector2i = ground.local_to_map(base.to_local(global_position))
 	print(apple,beta)
 	if abs(apple.x - beta.x) <= 1 and abs(apple.y - beta.y) <= 1:
-		# also have the broadcast move to the enemies here
-		# check for collisions
-		position = ground.map_to_local(ground.local_to_map(base.to_local(get_global_mouse_position())))
-		
+		var colorOfTile: Vector2i = ground.get_cell_atlas_coords(ground.local_to_map(base.to_local(get_global_mouse_position())))
+		# So far no textures in y coord
+		match colorOfTile.x:
+			1:
+				#red tile
+				pass
+			_:
+				# also have the broadcast move to the enemies here
+				position = ground.map_to_local(ground.local_to_map(base.to_local(get_global_mouse_position())))
