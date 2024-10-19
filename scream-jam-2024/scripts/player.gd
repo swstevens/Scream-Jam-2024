8f4e2b9bad7bb924b796aa4d945fe8fecebc7813
swstevens <sstevensw@gmail.com>
2024-10-19 16:46:18 -0700

    change location of enemy list update to better support spawners

--- scream-jam-2024/scripts/player.gd
+++ scream-jam-2024/scripts/player.gd
@@ -64,9 +64,7 @@ func _input(event):
 					return
 			
 			# Update the position of the character to where we clicked our mouse.
-			position = ground.map_to_local(apple)
-			enemies = get_tree().get_root().find_children("enemy*", "", true, false)
-			
+			position = ground.map_to_local(apple)			
 			# UPDATES
 
 			# Since we are moving our character, perform the enemy movements
@@ -74,6 +72,8 @@ func _input(event):
 				enemy.move(apple)
 			for spawner in spawners:
 				spawner.move(apple)
+			enemies = get_tree().get_root().find_children("enemy*", "", true, false)
+
 			updateEnemies(apple)
 				
 				
