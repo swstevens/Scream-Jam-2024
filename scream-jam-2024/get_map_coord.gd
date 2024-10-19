3ce5d97f0c837575c5e1c220387b0a7c3ec33959
Troy Clendenen <33107696+tclendo@users.noreply.github.com>
2024-10-18 18:37:36 -0700

    Add teleporter tile, teleporter functionality, and some comments

--- scream-jam-2024/get_map_coord.gd
+++ scream-jam-2024/get_map_coord.gd
@@ -2,9 +2,15 @@ extends Node2D
 
 @onready var Player: Node2D = $"player"
 @onready var Ground: TileMapLayer = $"Ground"
+var normalWalkTile: Vector2i = Vector2i(0,0)
 
 # key1 = 1, -4 : door1 = 4, -4
 var key1Pressed = false
+var key1Tile: Vector2i = Vector2i(1, -4)
+var door1Tile: Vector2i = Vector2i(4, -4)
+
+var teleporterA1 = Vector2i(-5, -1)
+var teleporterA2 = Vector2i(-5, -4)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,10 +18,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	var PlayerPos: Vector2 = Ground.local_to_map(to_local(Player.global_position))
-	if PlayerPos.x == 1 and PlayerPos.y == -4:
+	var PlayerPos: Vector2i = Ground.local_to_map(to_local(Player.global_position))
+	if PlayerPos == key1Tile:
 		if key1Pressed == false:
-			Ground.set_cell(Vector2i(4, -4), 0, Vector2i(0, 0), 0)
+			Ground.set_cell(door1Tile, 0, normalWalkTile, 0)
 		key1Pressed = true
-		print("Found key!")
-	pass
+
+func teleportPlayerTo() -> Vector2i:
+	match Ground.local_to_map(Player.position):
+		teleporterA1:
+			return teleporterA2
+		teleporterA2:
+			return teleporterA1
+		_:
+			# shouldn't hit this
+			return Ground.local_to_map(Player.position)
