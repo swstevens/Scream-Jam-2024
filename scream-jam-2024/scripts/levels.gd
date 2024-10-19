aae472605c67e0a2a919b62845b80ea2955dde5a
swstevens <sstevensw@gmail.com>
2024-10-19 10:13:12 -0700

    restructuring the project so it's not all in one folder

--- scream-jam-2024/scripts/levels.gd
+++ scream-jam-2024/scripts/levels.gd
@@ -3,5 +3,5 @@ extends Control
 
 
 func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/menus/menu.tscn")
 	pass # Replace with function body.
--- scream-jam-2024/scripts/menu.gd
+++ scream-jam-2024/scripts/menu.gd
@@ -3,17 +3,17 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://basic iso.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/basic iso.tscn")
 
 func _on_levels_pressed() -> void:
 	# hide initial screen and show the secondary one
-	get_tree().change_scene_to_file("res://levels.tscn")
+	get_tree().change_scene_to_file("res://scenes/menus/levels.tscn")
 
 	pass # Replace with function body.
 	
 func _on_options_pressed() -> void:
 	pass # Replace with function body.
-	get_tree().change_scene_to_file("res://options.tscn")
+	get_tree().change_scene_to_file("res://scenes/menus/options.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
